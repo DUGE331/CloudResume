3b72b4dff45eaf7b935c693627afd2be5951be2a
Isaac Douglas <douglasisaac74@gmail.com>
2026-02-25 14:52:58 +1100

    chore: clean up visitor counter and scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,51 @@
 /*!
-* Start Bootstrap - Resume v7.0.6 (https://startbootstrap.com/theme/resume)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)
+* Start Bootstrap - Resume v7.0.6
+* Licensed under MIT
 */
-//
-// Scripts
-// 
 
 window.addEventListener('DOMContentLoaded', event => {
 
-    // Activate Bootstrap scrollspy on the main nav element
+    // Activate Bootstrap scrollspy
     const sideNav = document.body.querySelector('#sideNav');
     if (sideNav) {
         new bootstrap.ScrollSpy(document.body, {
             target: '#sideNav',
             rootMargin: '0px 0px -40%',
         });
-    };
+    }
 
     // Collapse responsive navbar when toggler is visible
     const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
+    const responsiveNavItems = Array.from(
         document.querySelectorAll('#navbarResponsive .nav-link')
     );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
+    responsiveNavItems.forEach(item => {
+        item.addEventListener('click', () => {
             if (window.getComputedStyle(navbarToggler).display !== 'none') {
                 navbarToggler.click();
             }
         });
     });
+
+    // --- Visitor Counter ---
+    const apiUrl = "http://localhost:8000/visitor"; // local testing
+    // const apiUrl = "https://<your-api-gateway>.execute-api.ap-southeast-2.amazonaws.com/visitor"; // production
+
+    async function updateVisitorCount() {
+        try {
+            const response = await fetch(apiUrl); // GET increments counter
+            const data = await response.json();
+            const counter = document.getElementById("counter");
+            if (counter) {
+                counter.innerText = data.visitor_count; // match your API response
+            }
+        } catch (err) {
+            console.error("Visitor counter error:", err);
+            const counter = document.getElementById("counter");
+            if (counter) counter.innerText = "error";
+        }
+    }
+
+    updateVisitorCount();
 
 });
